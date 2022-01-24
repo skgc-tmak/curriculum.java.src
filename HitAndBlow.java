9d64ec8beedd35f48a6d694ccc58f06d7a34c352
skgc-tmak <t_sakaguchi@freestyles.jp>
2022-01-24 23:35:03 +0900

    fource commit

--- HitAndBlow.java
+++ HitAndBlow.java
@@ -5,36 +5,83 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HitAndBlow {
-    public static final boolean IS_TEST = true;
-    public static final int NUMBER_OF_DIGITS = 5;
-    public static final int[] BASE_NUMBER = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    public static final String MESSAGE_FOR_INPUT_INT = "数値で入力してください：";
-
     public static final Random RANDOM = new Random();
     public static final Scanner STDIN = new Scanner(System.in);
 
+    public static final boolean IS_TEST = true;
+    public static final int NUM_OF_DIGITS = 5;
+    public static final int[] BASE_NUM = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    public static final String MESSAGE_FOR_SUGGEST_INPUT_INT = "数値で入力してください：";
+    public static final int ONE_LINE = 1;
+    public static final String MESSAGE_FOR_INPUT_INT = "%d桁の数字を入力してください：";
+
     public static void main(String[] args) {
         List<Integer> elementNum = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        int[] nums = new int[NUMBER_OF_DIGITS];
-        generateNum(nums,elementNum);
-        testShowNum(nums, IS_TEST);
+        int[] dealerNums = new int[NUM_OF_DIGITS];
+        int[] playerNums = new int[NUM_OF_DIGITS];
+        generateNum(dealerNums, elementNum);
+        testShowNum(dealerNums, IS_TEST);
 
-        // receiveInputNumOfDigits();
+        showBlankLine(ONE_LINE);
 
+        showMessageInputNumDigit();
+
+        changeIntToArray(playerNums, receiveValidatedNum());
+
+        testShowNum(playerNums, IS_TEST);
     }
 
-    private static void receiveInputNumOfDigits() {
-        String inputStr = recieveInputStr();
+    private static void showMessageInputNumDigit() {
+        System.out.format(MESSAGE_FOR_INPUT_INT, NUM_OF_DIGITS);
+    }
 
-        int inputInt = validateStrToInt(inputStr);
+    private static void showBlankLine(int lines) {
+        for (int i = 0; i < lines; i++) {
+            System.out.println();
+        }
+    }
+
+    private static int receiveValidatedNum() {
+
+        int inputInt = validateStrToInt(recieveInputStr());
+
+        return validateDigit(inputInt);
+    }
+
+    private static int validateDigit(int inputInt) {
+        if (!isEqualNumOfDigits(changeIntToStr(inputInt))) {
+            showMessageInputNumDigit();
+            return receiveValidatedNum();
+        }
+        return inputInt;
+    }
+
+    private static String changeIntToStr(int num) {
+        return Integer.toString(num);
+    }
+
+    private static void changeIntToArray(int[] playerNums, int inputInt) {
+        for (int i = NUM_OF_DIGITS - 1; i > 0; i--) {
+            playerNums[i] = inputInt % 10;
+            inputInt /= 10;
+        }
+    }
+
+    private static boolean isEqualNumOfDigits(String str) {
+        return getStrLength(str) == NUM_OF_DIGITS;
+    }
+
+    private static int getStrLength(String inputStr) {
+        return inputStr.length();
     }
 
     private static int validateStrToInt(String inputStr) {
         int inputInt = 0;
         try {
-            changeStrToInt(inputStr);
-        } catch (Exception e) {
-            show(MESSAGE_FOR_INPUT_INT);
+            inputInt = changeStrToInt(inputStr);
+        } catch (NumberFormatException e) {
+            show(MESSAGE_FOR_SUGGEST_INPUT_INT);
+            return receiveValidatedNum();
         }
         return inputInt;
     }
@@ -60,14 +107,14 @@ public class HitAndBlow {
     }
 
     private static void generateNum(int[] nums, List<Integer> elementNum) {
-        for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
-            fetchRandomNum(i,nums,elementNum);
+        for (int i = 0; i < NUM_OF_DIGITS; i++) {
+            fetchRandomNum(i, nums, elementNum);
         }
     }
 
     private static void fetchRandomNum(int num, int[] array, List<Integer> elementNum) {
         int randomNum = getRandomNum(elementNum.size());
-        array[num]=getFromList(elementNum, randomNum);
+        array[num] = getFromList(elementNum, randomNum);
         removeFromList(elementNum, randomNum);
     }
 
